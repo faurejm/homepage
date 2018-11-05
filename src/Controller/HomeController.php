2115ba58032398167e6772c71ba92abbfae8cff7
faurejm <faure.jm@gmail.com>
2018-11-05 16:54:24 +0100

    V3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
-{
+{    /**
+    * @Route("/home/hello")
+    */
     public function index()
     {
         $hello = "Hello WCS";
